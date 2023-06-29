0453124c353edd59ead5915b88c8a9d1319b384f
SimarDhami <74151780+SimarDhami@users.noreply.github.com>
2023-06-29 22:16:57 +0530

    Time: 270 ms (19.30%), Space: 95.5 MB (12.76%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -1,22 +1,24 @@
 class Solution {
-    int hero(int ind, int buySell,vector<vector<int>> &dp, vector<int>& prices, int fee){
-        if(ind == prices.size()) return 0;
-        if(dp[buySell+1][ind] != -1)return dp[buySell+1][ind];
-        if (buySell == -1){
-            dp[buySell+1][ind] = max((-1 * prices[ind]) + hero(ind+1,0,dp,prices,fee), hero(ind+1,-1,dp,prices,fee));
-        } else {
-            if (buySell == 0) {
-                dp[buySell+1][ind] = max(prices[ind] - fee +  hero(ind+1, 1,dp, prices,fee), hero(ind+1, 0,dp, prices,fee));
-            } else {
-                dp[buySell+1][ind]= max((-1 * prices[ind]) + hero(ind+1,0,dp,prices,fee), hero(ind+1,1,dp,prices,fee));
-            }
-        }
-        return dp[buySell+1][ind];
-    }
 public:
+    int solve( vector<int> &prices, int i, int b, int fee, vector<vector<int>> &dp ) {
+        if( i == prices.size() )
+            return 0;
+        if( dp[i][b] != INT_MIN )
+            return dp[i][b];
+        
+        if( b ) {
+            dp[i][b] = max( solve(prices, i+1, b, fee, dp ), 
+                            -prices[i] + solve(prices, i+1, !b, fee, dp ) );
+        }
+        else {
+            dp[i][b] = max( solve(prices, i+1, b, fee, dp ), 
+                            prices[i]-fee + solve(prices, i+1, !b, fee, dp ) );
+        }
+        return dp[i][b];
+    }
+
     int maxProfit(vector<int>& prices, int fee) {
-        vector<vector<int>> dp(3, vector<int>(prices.size(),-1));
-        return hero(0,-1,dp,prices, fee);
-        // return dp[2][prices.size() - 1];
+        vector<vector<int>> dp( prices.size(), vector<int>( 2, INT_MIN ) );
+        return solve( prices, 0, 1, fee, dp );
     }
 };
